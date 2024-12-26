6cc5349b63f4fefc87f4a9b471f28098e380b925
bigpantherz <quynt.bc.2112@gmail.com>
2024-12-26 13:18:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-        function randomizePosition() {
-            const container = document.getElementById('container');
-            const maxX = window.innerWidth - container.offsetWidth; // Window width minus element width
-            const maxY = window.innerHeight - container.offsetHeight; // Window height minus element height
+function moveContainer() {
+    const container = document.getElementById('container');
+    const maxX = window.innerWidth - container.offsetWidth;
+    const maxY = window.innerHeight - container.offsetHeight;
 
-            const randomX = Math.random() * maxX;
-            const randomY = Math.random() * maxY;
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
 
-            container.style.position = 'absolute';
-            container.style.left = `${randomX}px`;
-            container.style.top = `${randomY}px`;
-        }
+    container.style.position = 'absolute';
+    container.style.left = `${randomX}px`;
+    container.style.top = `${randomY}px`;
+}
+
+function showCongratulations() {
+    alert('Chúc mừng! Bạn đã chọn trả nợ.');
+}
